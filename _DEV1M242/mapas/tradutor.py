ce46a6ceabfa050a7d8c3ac05d1dab67d4da21e2
lgmaciel <lgmaciel@email>
2025-06-11 11:58:36 -0300

    exemplos listas e mapas

---
+++ _DEV1M242/mapas/tradutor.py
@@ -0,0 +1,23 @@
+#
+# Model
+#
+
+dicionario = {
+    "ausência": "absence",
+    "budista": "buddhist",
+    "mamute": "mammoth",
+    "rinoceronte": "rhinoceros",
+    "meteorologista":"meteorologist",
+    "filo ": "phylum",
+    "falácia": "fallacy",
+    "destilação": "distillation",
+    "imobilizar": "immobilize",
+    "dálmata" : "dalmatian"
+}
+
+
+#
+# View
+#
+palavra = input("Palavra: ")
+print(dicionario[palavra])
